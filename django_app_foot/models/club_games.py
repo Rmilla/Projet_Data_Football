483cb86e2ff89d73c1d6961ed65ae1d451c15de0
Andy <andy@MacAir.local>
2024-01-11 12:48:54 +0100

    Commit import data

--- django_app_foot/models/club_games.py
+++ django_app_foot/models/club_games.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 class Club_games(models.Model):
-    #game_id
-    #club_id
+    game_id = models.IntegerField()
+    
+    club_id = models.IntegerField()
     own_goals = models.IntegerField()
     own_position = models.IntegerField()
-    own_manager_name = models.CharField()
-    #opponent_id
+    own_manager_name = models.CharField(max_length=100)
+    opponent_id = models.IntegerField()
     opponent_goals = models.IntegerField()
-    opponent_position = models.IntegerField()
+    opponents_position = models.IntegerField()
     opponent_manager_name = models.CharField(max_length=100)
     hosting = models.CharField(max_length=100)
+
+    def __str__(self) :
+        return f'{self.game_id}'
+
+    
