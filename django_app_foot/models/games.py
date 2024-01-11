6b5d642dfe9218ff55ab0b6622f30cc22a9b8c19
Robin MILLA <robin.milla.pro@gmail.com>
2024-01-11 17:35:18 +0100

    modifications modèles

--- django_app_foot/models/games.py
+++ django_app_foot/models/games.py
@@ -1,5 +1,5 @@
 from django.db import models
-from models import COMPETITION, HOME_CLUB, AWAY_CLUB
+from models import COMPETITION #HOME_CLUB, AWAY_CLUB
 
 class GAMES(models.Model):
     game_id = models.UUIDField(PrimaryKey=True)
@@ -10,5 +10,5 @@ class GAMES(models.Model):
     away_club_goals = models.IntegerField()
     home_club_position = models.IntegerField()
     competition_id = models.ForeignKey(COMPETITION, on_delete=models.CASCADE, related_name='competition')
-    home_club_id = models.ForeignKey(HOME_CLUB, on_delete=models.CASCADE, related_name='competition')
-    away_club_id = models.ForeignKey(AWAY_CLUB, on_delete=models.CASCADE, related_name='competition')
+    #home_club_id = models.ForeignKey(HOME_CLUB, on_delete=models.CASCADE, related_name='competition')
+    #away_club_id = models.ForeignKey(AWAY_CLUB, on_delete=models.CASCADE, related_name='competition')
