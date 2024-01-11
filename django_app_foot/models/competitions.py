a7407bcc8790cf1f8e48f6cbd9dc362ee04d465e
Robin MILLA <robin.milla.pro@gmail.com>
2024-01-11 09:51:11 +0100

    modification models

--- django_app_foot/models/competitions.py
+++ django_app_foot/models/competitions.py
@@ -1,4 +1,5 @@
 from django.db import models
+from models import COUNTRY
 
 class Competition(models.Model):
     CONFEDERATION = [
--- django_app_foot/models/games.py
+++ django_app_foot/models/games.py
@@ -1,4 +1,5 @@
 from django.db import models
+from models import COMPETITION, HOME_CLUB, AWAY_CLUB
 
 class Games(models.Model):
     season = models.IntegerField()
