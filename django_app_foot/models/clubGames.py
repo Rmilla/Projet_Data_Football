7b546fd8afc7e5f8894a8a334494abf5ed438f67
Robin MILLA <robin.milla.pro@gmail.com>
2024-01-15 14:37:45 +0100

    modification importData

--- django_app_foot/models/clubGames.py
+++ django_app_foot/models/clubGames.py
@@ -1,16 +1,20 @@
 from django.db import models
 
-class ClubGames(models.Model):
-    game_id = models.ForeignKey('Game', on_delete=models.CASCADE, related_name='games')
-    club_id = models.ForeignKey('Club', on_delete=models.CASCADE, related_name='clubs')
+
+class ClubGame(models.Model):
+    game = models.ForeignKey(
+        'Game', on_delete=models.CASCADE, related_name='clubGames')
+    club = models.ForeignKey(
+        'Club', on_delete=models.CASCADE, related_name='clubGames')
     own_goals = models.IntegerField()
     own_position = models.FloatField(null=True, blank=True)
     own_manager_name = models.CharField(max_length=100)
-    opponent_id = models.ForeignKey('Club', on_delete=models.CASCADE, related_name='opponent_club')
+    opponent_id = models.ForeignKey(
+        'Club', on_delete=models.CASCADE, related_name='opponent_club')
     opponent_goals = models.IntegerField()
     opponent_position = models.FloatField(null=True, blank=True)
     opponent_manager_name = models.CharField(max_length=100)
     hosting = models.CharField(max_length=100)
 
-    def __str__(self) :
-        return f'{self.game_id}'
+    def __str__(self):
+        return f'{self.game}'
--- django_app_foot/models/competition.py
+++ django_app_foot/models/competition.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Competition(models.Model):
-    competition_id = models.CharField(primary_key=True)
+    competition_id = models.CharField(primary_key=True,max_length=100)
     CONFEDERATION = [
         ("EU", "europa")
     ]
-    competition_code = models.CharField(max_length=5)
-    name = models.CharField()
-    sub_type = models.CharField()
-    type = models.CharField()
-    country_name = models.CharField()
-    domestic_league_code = models.CharField()
-    confederation = models.CharField(choices=CONFEDERATION)
+    competition_code = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    sub_type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    country_name = models.CharField(max_length=100)
+    domestic_league_code = models.CharField(max_length=100)
+    confederation = models.CharField(choices=CONFEDERATION,max_length=100)
     url = models.URLField()
     country_id = models.IntegerField()
