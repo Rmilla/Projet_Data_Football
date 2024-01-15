21f73d599c12f881b4e57c28b56d03887655f3d3
Robin MILLA <robin.milla.pro@gmail.com>
2024-01-15 14:42:16 +0100

    modification __init__

--- django_app_foot/models/__init__.py
+++ django_app_foot/models/__init__.py
@@ -2,7 +2,7 @@
 from .competition import Competition
 from .game import Game
 from .appearances import Appearance
-from .clubGames import ClubGames
+from .clubGames import ClubGame
 from .gameEvent import GameEvent
 from .playersValuation import PlayersValuation
 
