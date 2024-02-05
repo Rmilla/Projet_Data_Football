9b449d9014cc6c5f850b3bff3d2671ccf76043c5
Chelvi Benjamin <chelvibenjamin@gmail.com>
2024-02-05 21:02:35 +0100

    ajout de la logique de filtrage en back

--- django_app_foot/views/competitions.py
+++ django_app_foot/views/competitions.py
@@ -1,25 +1,21 @@
 from rest_framework import serializers, viewsets, permissions, status
-from django_app_foot.models import Competition
+from django_app_foot.models import Competition, Club
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from ..pagination import MyPaginationClass
 from .club import ClubSerializer
 from .games import GameSerializer
 
+
 class CompetitionSerializer(serializers.ModelSerializer):
     clubs = ClubSerializer(many=True, read_only=True)
-    has_clubs = filters.BooleanFilter(method='filter_has_clubs', label='Has Clubs')
 
-    def filter_has_clubs(self, queryset, name, value):
-        if value:
-            return queryset.filter(clubs__isnull=False).distinct()
-        else:
-            return queryset.filter(clubs__isnull=True)
    
     class Meta: 
 
         model = Competition
         fields = "__all__"
+
 
 class CompetitionFilter(filters.FilterSet):
  
@@ -36,7 +32,7 @@ class CompetitionFilter(filters.FilterSet):
         }
 class CompetitionViewSet(viewsets.ModelViewSet):
   
-    queryset = Competition.objects.all()
+    queryset = Competition.objects.filter(clubs__gt=0) #on rajoute un filtre pour ne renvoyer que les competitions qui possèdent des clubs associés
     serializer_class = CompetitionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     # pagination_class = MyPaginationClass
