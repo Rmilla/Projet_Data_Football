22f57ac3e86654268f6688b9195b522e38750794
ncs <ncs@ncs.local>
2024-02-06 12:18:34 +0100

    Message de commit

--- django_app_foot/views/competitions.py
+++ django_app_foot/views/competitions.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewsets, permissions, status
+from rest_framework import serializers, viewsets, permissions
 from django_app_foot.models import Competition, Club
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -6,35 +6,33 @@ from ..pagination import MyPaginationClass
 from .club import ClubSerializer
 from .games import GameSerializer
 
-
 class CompetitionSerializer(serializers.ModelSerializer):
     clubs = ClubSerializer(many=True, read_only=True)
 
-   
-    class Meta: 
-
+    class Meta:
         model = Competition
         fields = "__all__"
 
-
 class CompetitionFilter(filters.FilterSet):
- 
+  
     class Meta:
         model = Competition
         fields = {
-            "competition_id": ['icontains', 'contains','exact'],
-            "competition_code": ['icontains', 'contains','exact'],
-            "name": ['icontains', 'contains','exact'],
-            "type": ['icontains', 'contains','exact'],
+            "competition_id": ['icontains', 'contains', 'exact'],
+            "competition_code": ['icontains', 'contains', 'exact'],
+            "name": ['icontains', 'contains', 'exact'],
+            "type": ['icontains', 'contains', 'exact'],
             "country_name": ['icontains', 'contains'],
             "domestic_league_code": ['icontains', 'contains'],
             "confederation": ['icontains', 'contains'],
         }
+
+    def filter_last_season(self, queryset, name, value):
+        return queryset.filter(clubs__last_season=value).distinct()
+
 class CompetitionViewSet(viewsets.ModelViewSet):
-  
-    queryset = Competition.objects.filter(clubs__gt=0) #on rajoute un filtre pour ne renvoyer que les competitions qui possèdent des clubs associés
+    queryset = Competition.objects.filter(clubs__gt=0)
     serializer_class = CompetitionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # pagination_class = MyPaginationClass
     filter_backends = [DjangoFilterBackend]
-    filterset_class = CompetitionFilter 
+    filterset_class = CompetitionFilter
